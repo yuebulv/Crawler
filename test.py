8946f3366aaf20f85740f57a33f1e53aa7cd6fcb
lyc <you@example.com>
2021-01-07 21:55:23 +0800

    V1.0 add dynamic prj_page;next step:analysis remark

--- test.py
+++ test.py
@@ -1,20 +1,6 @@
 import requests
 if __name__=="__main__":
-    #1.获得公司名称及ID
-    headers={
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
-    }
-    url='https://glxy.mot.gov.cn/company/getCompanyAchieveInfo.do'
-    kw='b'#input('enter a word:')
-    param={
-        'id': 'f51cb26b665b428d8f807745793b955b',
-        'companyid': '596b4e9580f94532ad6c066f3701aa07',
-    }
-    page_text=requests.post(url=url,headers=headers,data=param,verify=False).json()
-    # page_text = requests.post(url=url, headers=headers, verify=False).json()
-    print(page_text)
-    # id_list=[]
-    # corpName_list = []
-    # for dic in page_text['data']:
-    #     id_list.append(dic['id'])
-    #     corpName_list.append(dic['remark'])
+    url='https://wwww.qiushibike.com/pic/page/%d/?s='
+    for pagenum in range(1,10):
+        new_url=format(url%pagenum)
+        print(new_url)
