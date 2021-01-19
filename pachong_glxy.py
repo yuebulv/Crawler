1d743a12f7d497a3e3184af2822b1ad0ddd1a9d3
lyc <you@example.com>
2021-01-19 15:52:05 +0800

    add function trans EI DAT file to Hint 3dr fiel

--- pachong_glxy.py
+++ pachong_glxy.py
@@ -1,32 +1,6 @@
 import requests
 import json
 import time
-def get_corp_id(page):
-
-    # 获取公司ID
-    headers={
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
-    }
-    url='https://glxy.mot.gov.cn/company/getCompanyAptitude.do'
-    kw='b'#input('enter a word:')
-    param={
-        'page': page,
-        'rows': '15',
-        'type': '2',
-        'text':'',
-    }
-    page_text=requests.post(url=url,headers=headers,data=param,verify=False).json()
-    id_list=[]
-    corpName_list = []
-    for dic in page_text['rows']:
-        id_list.append(dic['id'])
-        corpName_list.append(dic['corpName'])
-    filename=kw+'.txt'
-    with open(filename,'w',encoding='utf-8') as fp:
-        fp.write(id_list)
-def get_prj_id():
-    # 获取项目ID
-    pass
 if __name__=="__main__":
     start_time = time.time()
     #1.获得公司名称及ID
@@ -35,52 +9,55 @@ if __name__=="__main__":
     }
     url='https://glxy.mot.gov.cn/company/getCompanyAptitude.do'
     kw='b'#input('enter a word:')
-    param={
-        'page': '1',
-        'rows': '15',
-        'type': '2',
-        'text':'',
-    }
-    page_text=requests.post(url=url,headers=headers,data=param,verify=False).json()
-    id_list=[]
-    corpName_list = []
-    for dic in page_text['rows']:
-        id_list.append(dic['id'])
-        corpName_list.append(dic['corpName'])
-    #2.获得公司设计项目
-    for id in id_list:
-        for prj_page in range(1,10):
-            url = 'https://glxy.mot.gov.cn/company/getCompanyAchieveList.do?companyId='+id+'&type=1a'
-            # url='https://glxy.mot.gov.cn/company/getCompanyAchieveList.do?companyId=596b4e9580f94532ad6c066f3701aa07&type=1a'
-            data={
-                'page': prj_page,
-                'rows':'15',
-                'sourceInfo':'1',
-            }
-            page_text = requests.post(url=url, headers=headers, data=data, verify=False).json()
-            prj_list = []
-            prjid_list = []
-            companyid_list = []
-            companyname_list = []
-            company_prjid_list = []
-            company_prjname_list = []
-            remark_list = []
-            for dic in page_text['rows']:
-                prj_list.append(dic['projectName'])
-                prjid_list.append(dic['id'])
-            # 3.获得公司设计项目ramark内容
-            for prjid in prjid_list:
-                prj_url='https://glxy.mot.gov.cn/company/getCompanyAchieveInfo.do'
-                prjdata={
-                    'id': prjid,
-                    'companyid': id,
+    for corp_page in range(1, 4):
+        param={
+            'page': corp_page,
+            'rows': '15',
+            'type': '2',
+            'text':'',
+        }
+        page_text=requests.post(url=url,headers=headers,data=param,verify=False).json()
+        id_list=[]
+        corpName_list = []
+        for dic in page_text['rows']:
+            id_list.append(dic['id'])
+            corpName_list.append(dic['corpName'])
+        #2.获得公司设计项目
+        for id in id_list:
+            for prj_page in range(1,2):
+                url = 'https://glxy.mot.gov.cn/company/getCompanyAchieveList.do?companyId='+id+'&type=1a'
+                # url='https://glxy.mot.gov.cn/company/getCompanyAchieveList.do?companyId=596b4e9580f94532ad6c066f3701aa07&type=1a'
+                data={
+                    'page': prj_page,
+                    'rows':'15',
+                    'sourceInfo':'1',
                 }
-                prjcont= requests.post(url=prj_url, headers=headers, data=prjdata, verify=False).json()
-                res=prjcont['data']
-                res['remark']=res['remark'].replace('\n','')
-                res['remark']=res['remark'].replace('\r', '')
-                temp = '\n'+res['companyId']+" "+res['corpName']+" 初步设计批复时间："+res['designAllowDate']+" 施工图批复时间："+res['drawingAllowDate']+" "+res['id']+" "+res['projectType']+" "+res['projectName']+" "+res['remark']
-                filename=kw+'.txt'
-                with open(filename,'a',encoding='utf-8') as fp:
-                    fp.write(str(temp))
+                page_text = requests.post(url=url, headers=headers, data=data, verify=False).json()
+                prj_list = []
+                prjid_list = []
+                companyid_list = []
+                companyname_list = []
+                company_prjid_list = []
+                company_prjname_list = []
+                remark_list = []
+                for dic in page_text['rows']:
+                    prj_list.append(dic['projectName'])
+                    prjid_list.append(dic['id'])
+                # 3.获得公司设计项目ramark内容
+                for prjid in prjid_list:
+                    prj_url='https://glxy.mot.gov.cn/company/getCompanyAchieveInfo.do'
+                    prjdata={
+                        'id': prjid,
+                        'companyid': id,
+                    }
+                    prjcont= requests.post(url=prj_url, headers=headers, data=prjdata, verify=False).json()
+                    res=prjcont['data']
+                    res['remark']=res['remark'].replace('\n','')
+                    res['remark']=res['remark'].replace('\r', '')
+                    temp = '\n'+res['companyId']+" "+res['corpName']+" 初步设计批复时间："+res['designAllowDate']+" 施工图批复时间："+res['drawingAllowDate']+" "+res['id']+" "+res['projectType']+" "+res['projectName']+" "+res['remark']
+                    filename=kw+'.txt'
+                    with open(filename,'a',encoding='utf-8') as fp:
+                        fp.write(str(temp))
+        print("公司第"+str(corp_page)+"页完成")
+        print("用时：%f" % (time.time() - start_time))
     print("用时：%f" % (time.time()-start_time))
--- test.py
+++ test.py
@@ -1,6 +1,38 @@
 import requests
-if __name__=="__main__":
+
+
+def str_connect():
     url='https://wwww.qiushibike.com/pic/page/%d/?s='
     for pagenum in range(1,10):
         new_url=format(url%pagenum)
         print(new_url)
+
+def str_connect():
+    page=1
+    print("第"+str(page)+"页")
+
+def text1():
+    headers={
+        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+    }
+    url='https://glxy.mot.gov.cn/company/getCompanyAptitude.do'
+    kw='b'#input('enter a word:')
+    for corp_page in range(1, 8):
+        param={
+            # 'page': corp_page,
+            # 'rows': 15,
+            # 'type': 2,
+            # 'regProvinceCode':"",
+            # 'catype':"",
+            # 'grade':"",
+            # 'text':"",
+            'page': corp_page,
+            'rows': '15',
+            'type': '2',
+            'text': '',
+
+        }
+        page_text=requests.post(url=url,headers=headers,data=param,verify=False).json()
+        print(page_text)
+if __name__=="__main__":
+    temp=text1()
